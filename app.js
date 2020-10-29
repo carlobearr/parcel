88fef80d17a507d60820eeae3bf8f68dda0fe652
Carlo Santos <carlo_luis_v_santos@dlsu.edu.ph>
2020-10-29 14:54:37 +0800

    fixed port

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ app.get('/', (req,res) => {
     res.send("WTF");
 })
 
-app.listen(3000, () => {
+const port = process.env.port || 3000;
+app.listen(port, () => {
     console.log("starting at localhost:3000");
 })
