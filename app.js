96464ac46813f6097ae1036e0c1279e962245392
Carlo Santos <carlo_luis_v_santos@dlsu.edu.ph>
2020-11-02 13:17:34 +0800

    development branch test

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req,res) => {
-    res.send("TEST");
+    res.send("DEVELOPMENT BRANCH");
 })
 
 const port = process.env.port || 3000;
