fae1f98c8f0edffa9eeea504d9e7711f85a50128
ronellroxas <61377760+ronellroxas@users.noreply.github.com>
2020-11-17 20:33:33 +0800

    Added tracking page
    Added tracking page component and css

--- frontend/src/pages/trackingPage.js
+++ frontend/src/pages/trackingPage.js
@@ -1,9 +1,46 @@
 import React from 'react';
+import { Row, Col, Timeline } from 'antd';
+import './trackingPage.css';
 
 function Tracking() {
 
     return ( 
-        <h1> Tracking </h1>
+        <div> 
+            <Row> 
+                <Col span={12}>                           
+                    <div className="tracking-header"> Tracking </div>
+                    <div className="tracking-number"> Tracking Number </div>
+                    <Timeline mode="right">
+                        <Timeline.Item label="Status is here" color="blue">
+                            <p className="tracking-date">MM YY DD</p>
+                            <p>HH MM SS</p>
+                        </Timeline.Item>
+                        <Timeline.Item label="Status is here" color="blue">
+                            <p className="tracking-date">MM YY DD</p>
+                            <p>HH MM SS</p>
+                        </Timeline.Item>
+                        <Timeline.Item label="Status is here" color="blue">
+                            <p className="tracking-date">MM YY DD</p>
+                            <p>HH MM SS</p>
+                        </Timeline.Item>
+                        <Timeline.Item label="Status is here" color="blue">
+                            <p className="tracking-date">MM YY DD</p>
+                            <p>HH MM SS</p>
+                        </Timeline.Item>
+                        <Timeline.Item label="Status is here" color="gray">
+                            <p className="tracking-date">MM YY DD</p>
+                            <p>HH MM SS</p>
+                        </Timeline.Item>
+                        <Timeline.Item label="Status is here" color="gray">
+                            <p className="tracking-date">MM YY DD</p>
+                            <p>HH MM SS</p>
+                        </Timeline.Item>
+                    </Timeline>
+                </Col>
+                <Col span={12} className="tracking-side">
+                </Col>
+            </Row>
+        </div>
     );
 };
 
