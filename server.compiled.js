d9e8ff39019cb09599e894a26345eee2b4b63d8c
Carlo Santos <carlo_luis_v_santos@dlsu.edu.ph>
2020-12-03 16:40:06 +0800

    try

--- server.compiled.js
+++ server.compiled.js
@@ -1,23 +1,53 @@
 "use strict";
 
-// server.js
-//
+require('dotenv').config();
+
 var express = require('express');
 
-var path = require('path');
+var bodyParser = require('body-parser');
+
+var session = require('express-session');
+
+var mongoose = require('mongoose');
+
+var MongoStore = require('connect-mongo')(session); //const cors = require('cors');
+//routes
+
+
+var adminRoute = require('./routes/adminRoute');
+
+var userRoute = require('./routes/userRoute'); //app settings
+
 
 var app = express();
-var PORT = process.env.HTTP_PORT || 4001;
-app.use(express["static"](path.join(__dirname, 'frontend', 'build')));
-app.get('/', function (req, res) {
-  res.send('just gonna send it');
-});
-app.get('/flower', function (req, res) {
-  res.json({
-    name: 'Dandelion',
-    colour: 'Blue-ish'
-  });
-});
-app.listen(PORT, function () {
-  console.log("Server listening at port ".concat(PORT, "."));
-});
+var port = process.env.PORT || 8080;
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+  extended: true
+})); // app.use(cors({
+//     credentials: true,
+//     origin: "http://localhost:3000"
+// }));
+//db connection for app (place before sessions for cookie persistence)
+
+var connection = require('./db');
+
+connection(); //session settings
+
+app.use(session({
+  name: process.env.SESSION_NAME,
+  secret: process.env.SESSION_SECRET,
+  store: new MongoStore({
+    mongooseConnection: mongoose.connection
+  }),
+  resave: false,
+  saveUninitialized: true,
+  maxAge: 1000 * 60 * 30 //30 mins
+
+}));
+app.listen(port, function () {
+  console.log("API running at localhost:" + port);
+}); //use routes
+
+app.use('/', adminRoute);
+app.use('/user', userRoute);
