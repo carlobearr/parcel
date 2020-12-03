a24ef4853cea069697e49b74b9a7841e477723fe
Carlo Santos <carlo_luis_v_santos@dlsu.edu.ph>
2020-12-03 14:40:13 +0800

    adsa

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,31 +7,6 @@ import Routes from './Routes';
 import './App.css';
 import {getSessionUser} from './api/sessionHandler';
 
-<<<<<<< HEAD
-class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      flower: {}
-    }
-    this.getFlower();
-  }
-  getFlower() {
-    fetch("/flower")
-      .then(response => response.json())
-      .then(data => {
-        this.setState({
-          flower: data
-        });
-      });
-  }
-  render() {
-    return (
-      <div className="App">
-        <h1>{this.state.flower.name}</h1>
-        <p>{this.state.flower.colour}</p>
-      </div>
-=======
 const { Content } = Layout;
 
 const App = () => {
@@ -56,7 +31,6 @@ const App = () => {
             </Content>
             <LandingFooter></LandingFooter>
         </Layout>
->>>>>>> development-clone
     );
   }
 }
