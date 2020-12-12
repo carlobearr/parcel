f0222ddfab3bef0f32e6303cf58a41af9fc9b3ce
Carlo Santos <carlo_luis_v_santos@dlsu.edu.ph>
2020-12-12 19:39:19 +0800

    changed routes

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -3,10 +3,10 @@ const userController = require('../controllers/userController');
 
 const router = express.Router();
 
-router.post('/login', userController.loginUser);
+router.post('/api/login', userController.loginUser);
 
-router.delete('/logout', userController.logoutUser);
+router.delete('/api/logout', userController.logoutUser);
 
-router.get('/getSession', userController.getSessionUser);
+router.get('/api/getSession', userController.getSessionUser);
 
 module.exports = router;
