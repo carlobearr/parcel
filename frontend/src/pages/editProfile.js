016185baecaeb309f6e34101ad466154875ede4a
ronellroxas <61377760+ronellroxas@users.noreply.github.com>
2021-01-22 17:58:22 +0800

    editProfile initial value update

--- frontend/src/pages/editProfile.js
+++ frontend/src/pages/editProfile.js
@@ -2,7 +2,7 @@ import React from 'react';
 import {Row, Col, Form, Input, Button} from 'antd';
 import './editProfile.css';
 
-function Tracking() {
+function EditProfile(props) {
     return (
         <div className="editProfilePage">
             <div className="editProfileMain">
@@ -13,16 +13,13 @@ function Tracking() {
                         </Col>
                     </Row>
                     <Row gutter={4}>
-                        <Col span={6}>
-                            <Form.Item label="First name" name="firstName"><Input/></Form.Item>
-                        </Col>
-                        <Col span={6}>
-                            <Form.Item label="Last name" name="lastName"><Input/></Form.Item>
+                        <Col span={12}>
+                            <Form.Item label="Full name" name="fullName" initialValue={props.isLoggedIn.name}><Input/></Form.Item>
                         </Col>
                     </Row>
                     <Row gutter={4}>
                         <Col span={12}>
-                            <Form.Item label="Email Address" name="email"><Input/></Form.Item>
+                            <Form.Item label="Email Address" name="email" initialValue={props.isLoggedIn.email}><Input/></Form.Item>
                         </Col>
                     </Row>
                     <Row gutter={4}>
@@ -41,4 +38,4 @@ function Tracking() {
     );
 }
 
-export default Tracking;
+export default EditProfile;
