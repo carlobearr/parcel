13855f580c428fe07563ec873d21460faba74454
ronellroxas <61377760+ronellroxas@users.noreply.github.com>
2021-01-23 20:26:34 +0800

    edit profile conflict fix

--- frontend/src/pages/editProfile.js
+++ frontend/src/pages/editProfile.js
@@ -1,43 +1,50 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {Row, Col, Form, Input, Button, Avatar} from 'antd';
 import './editProfile.css';
+import {editUser} from '../api/profileHandler';
 
 function EditProfile(props) {
+
+    const [update, setUpdate] = useState("");
+
+    const submitEdit = (formVal) => {
+        async function updateUser() {
+            const result = await editUser(formVal);
+
+            if(result.updated) {
+                setUpdate("Updated!");
+            }
+            else if(result.err) {
+                setUpdate("Update failed, try again later.");
+            }
+            
+        }
+
+        updateUser();
+    }
+
     return (
         <div className="editProfilePage">
             <div className="editProfileMain">
-                <Form layout="vertical">
+                <Form layout="vertical" onFinish={submitEdit}>
                     <Row>
                         <Col span={16}>
                             <Row>
                                 <p className="myAddresses">Edit Profile</p>
                             </Row>
-                            <Row gutter={[20,15]}>
-                                <Col span={14}>
-                                    <Form.Item label="First Name" name="firstName" initialValue={props.isLoggedIn.name}><Input/></Form.Item>
-                                </Col>
-                                <Col span={10}>
-                                    <Form.Item label="Last Name" name="lastName" initialValue={props.isLoggedIn.name}><Input/></Form.Item>
+                            <Row gutter={[0,15]}>
+                                <Col span={20}>
+                                    <Form.Item label="Full Name" name="name" initialValue={props.isLoggedIn.name}><Input/></Form.Item>
                                 </Col>
                             </Row>
                             <Row gutter={[0,15]}>
-                                <Col span={24}>
+                                <Col span={20}>
                                     <Form.Item label="Email Address" name="email" initialValue={props.isLoggedIn.email}><Input/></Form.Item>
                                 </Col>
                             </Row>
                             <Row gutter={[0,15]}>
-                                <Col span={15}>
-                                    <Form.Item label="Cellphone Number" name="cellphoneNumber"><Input/></Form.Item>
-                                </Col>
-                            </Row>
-                            <Row gutter={[0,15]}>
-                                <Col span={24}>
-                                    <Form.Item label="New Password" name="newPassword"><Input/></Form.Item>
-                                </Col>
-                            </Row>
-                            <Row gutter={[0,26]}>
-                                <Col span={24}>
-                                    <Form.Item label="Confirm Password" name="confirmPassword"><Input/></Form.Item>
+                                <Col span={20}>
+                                    <Form.Item label="Cellphone Number" name="cellphoneNumber" initialValue={props.isLoggedIn.cellphoneNumber}><Input placeholder="Not required."/></Form.Item>
                                 </Col>
                             </Row>
                         </Col>
@@ -49,7 +56,8 @@ function EditProfile(props) {
                     </Row>
                     <Col offset={17}>
                         <div className="profileCenter">
-                            <Button className="submitRight">SAVE PROFILE</Button>
+                            <p className="edit-updates">{update}</p>
+                            <Button className="submitRight" htmlType="submit">SAVE PROFILE</Button>
                         </div>
                     </Col>
                 </Form>
